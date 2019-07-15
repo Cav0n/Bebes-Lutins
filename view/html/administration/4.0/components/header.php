fc7376b30cd62a16a2549602c24fe33c2fa7b7d5
cav0n <cav0n@hotmail.fr>
2019-07-15 11:47:43 +0200

    Updated dashboard version number 4.3.0

--- view/html/administration/4.0/components/header.php
+++ view/html/administration/4.0/components/header.php
@@ -35,7 +35,7 @@ if($_GET['page'] == "nouveau" || $_GET['page'] == "edition" || $_GET['page'] == 
         <li class="<?php if($_GET['section'] == "analyses") echo 'selected'; ?>"><a href="https://www.bebes-lutins.fr/dashboard4/analyses/tableau-de-bord/tous"><div class="horizontal"><?php echo file_get_contents("view/assets/images/utils/icons/analysis.svg"); ?></div><p>Analyses</p></a></li>
     </ul>
     <ul id="version-container">
-        <li>Version : 4.2.1</li>
+        <li>Version : 4.3.0</li>
     </ul>
 </div>
 

a439fe0d80dbd234ae2b16fdf630239b5934f79c
Florian BERNARD <Cav0n@hotmail.fr>
2019-07-17 13:52:27 +0200

    Creating WishList model + updated gatewat

--- model/gateways/WishListGateway.php
+++ model/gateways/WishListGateway.php
@@ -2,19 +2,31 @@
 
 class WishListGateway
 {
-    public static function GetWishListOfUser(string $user_id){
+    /**
+     * @param string user_id Identifiant de l'utilisateur
+     */
+    public static function GetWishListOfUser(string $user_id): WishList{
         global $dblogin, $dbpassword, $dsn;
         $con = new Connexion($dsn, $dblogin, $dbpassword);
 
         $query = "SELECT id, user_id, message FROM wishlist WHERE user_id=:user_id;";
         $con->executeQuery($query, array(':user_id'=>array($user_id, PDO::PARAM_STR)));
-        $wishlist = $con->getResults()[0];
+        $results = $con->getResults()[0];
+        $wishlist = new WishList($results['id'], $results['user_id'], $results['message']); 
 
         if($wishlist != null){
             $query = "SELECT id, wishlist_id, product_id, message FROM wishlist_items WHERE wishlist_id=:wishlist_id;";
             $con->executeQuery($query, array(':wishlist_id'=>array($wishlist['id'], PDO::PARAM_STR)));
-            $wishlist_items = $con->getResults();
+            $results = $con->getResults();
+
+            foreach ($results as $result) {
+                $wishlist_items[] = new WishListItem($result['id'], $result['wishlist_id'], $result['product_id'], $result['message']);
+            }
+
+            $wishlist->setItems($wishlist_items);
         }
+
+        return $wishlist;
     }
 
     /**
@@ -25,6 +37,24 @@ class WishListGateway
         global $dblogin, $dbpassword, $dsn;
         $con = new Connexion($dsn, $dblogin, $dbpassword);
 
-        //$query = "INSERT INTO "
+        $query = "INSERT INTO whishlist_items VALUES(:id, :wishlist_id, :product_id, 1);";
+        $con->executeQuery($query, array(
+            ':id' => array(uniqid('item_'), PDO::PARAM_STR),
+            ':wishlist_id' => array($wishlist_id, PDO::PARAM_STR),
+            ':product_id' => array($product_id, PDO::PARAM_STR)
+        ));
+    }
+
+    /**
+     * @param string id Identifiant de la liste d'envie
+     */
+    public static function DeleteItemToWishlist(string $id){
+        global $dblogin, $dbpassword, $dsn;
+        $con = new Connexion($dsn, $dblogin, $dbpassword);
+
+        $query = "DELETE FROM whishlist_items WHERE id=:id;";
+        $con->executeQuery($query, array(
+            ':id' => array($id)
+        ));
     }
 }

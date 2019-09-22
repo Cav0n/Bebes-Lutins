f923b28b663fbc8e0876e9f406d521afcd7ce663
Florian Bernard <florianbernard@macbook-pro-de-florian.home>
2019-09-22 15:05:01 +0200

    Updated footer

--- resources/views/layouts/public/footer.blade.php
+++ resources/views/layouts/public/footer.blade.php
@@ -1,142 +1,45 @@
-<!-- Footer -->
-<footer class="page-footer font-small unique-color-dark">
-
-    <div class='bg-warning'>
-      <div class="container">
-  
-        <!-- Grid row-->
-        <div class="row py-4 d-flex align-items-center">
-  
-          <!-- Grid column -->
-          <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
-            <h6 class="mb-0" style='color:white;'>Suivez nous sur nos réseaux sociaux</h6>
-          </div>
-          <!-- Grid column -->
-  
-          <!-- Grid column -->
-          <div class="col-md-6 col-lg-7 text-center text-md-right" style='color:white;'>
-  
-            <!-- Facebook -->
-            <a class="fb-ic">
-              <i class="fab fa-facebook-f white-text mr-4"> </i>
-            </a>
-            <!-- Twitter -->
-            <a class="tw-ic">
-              <i class="fab fa-twitter white-text mr-4"> </i>
-            </a>
-            <!-- Google +-->
-            <a class="gplus-ic">
-              <i class="fab fa-google-plus-g white-text mr-4"> </i>
-            </a>
-            <!--Linkedin -->
-            <a class="li-ic">
-              <i class="fab fa-linkedin-in white-text mr-4"> </i>
-            </a>
-            <!--Instagram-->
-            <a class="ins-ic">
-              <i class="fab fa-instagram white-text"> </i>
-            </a>
-  
-          </div>
-          <!-- Grid column -->
-  
-        </div>
-        <!-- Grid row-->
-  
-      </div>
+<footer class='page-footer font-small bg-dark text-light'>
+  <div class='footer-copyright text-center py-3'>
+    © 2019 Bébés Lutins
+  </div>
+  <div class='row justify-content-center'>
+    <div class="col-3">
+      <p class="h5">Infos pratiques</p>
+      <ul class='list-unstyled'>
+        <li><a href='/livraison-et-frais-de-ports' class='text-light'>Livraison et frais de ports</a></li>
+        <li><a href='/paiement' class='text-light'>Paiement</a></li>
+        <li><a href='/echanges-et-retours' class='text-light'>Échanges et retours</a></li>
+        <li><a href='/mentions-legales' class='text-light'>Mentions légales</a></li>
+        <li><a href='/conditions-de-ventes' class='text-light'>Conditions de ventes</a></li>
+        <li><a href='/plan-du-site' class='text-light'>Plan du site</a></li>
+      </ul>
     </div>
-  
-    <!-- Footer Links -->
-    <div class="container text-center text-md-left mt-5">
-  
-      <!-- Grid row -->
-      <div class="row mt-3">
-  
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-  
-          <!-- Content -->
-          <h6 class="text-uppercase font-weight-bold">Bébés Lutins</h6>
-          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-          <p>Nous fabriquons des couches lavables dans notre atelier en France.</p>
-  
-        </div>
-        <!-- Grid column -->
-  
-        <!-- Grid column -->
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-  
-          <!-- Links -->
-          <h6 class="text-uppercase font-weight-bold">Nos services</h6>
-          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-          <p>
-            <a href="#!">Service 1</a>
-          </p>
-          <p>
-            <a href="#!">Service 2</a>
-          </p>
-          <p>
-            <a href="#!">Service 3</a>
-          </p>
-          <p>
-            <a href="#!">Service 4</a>
-          </p>
-  
-        </div>
-        <!-- Grid column -->
-  
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-  
-          <!-- Links -->
-          <h6 class="text-uppercase font-weight-bold">Infos pratiques</h6>
-          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-          <p>
-            <a href="#!">Livraisons et frais de port</a>
-          </p>
-          <p>
-            <a href="#!">Paiement</a>
-          </p>
-          <p>
-            <a href="#!">Échanges et retours</a>
-          </p>
-          <p>
-            <a href="#!">Mentions légales</a>
-          </p>
-          <p>
-            <a href="#!">Conditions de vente</a>
-          </p>
-          <p>
-            <a href="#!">Plan du site</a>
-          </p>
-  
-        </div>
-        <!-- Grid column -->
-  
-        <!-- Grid column -->
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-  
-          <!-- Links -->
-          <h6 class="text-uppercase font-weight-bold">Nous contacter</h6>
-          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-          <p>Rue du 19 mars 1962<BR>63300<BR>THIERS</p>
-          <p>
-            <i class="fas fa-envelope mr-3"></i>actypoles@actypoles-thiers.fr</p>
-          <p>
-            <i class="fas fa-phone mr-3"></i>04 73 80 26 60</p>
-  
-        </div>
-        <!-- Grid column -->
-  
-      </div>
-      <!-- Grid row -->
-  
+    <div class="col-3">
+      <p class="h5">Nous contacter</p>
+      <ul class="list-unstyled">
+        <li><a href='tel:+33641569165' class='text-light'>06 41 56 91 65</a></li>
+        <li><a href='mailto:contact@bebes-lutins.fr' class='text-light'>contact@bebes-lutins.fr</a></li>
+        <li><a href='/contact' class='text-light'>Formulaire de contact</a></li>
+        <li class="mt-3">Katia répondra avec plaisir à vos questions.</li>
+      </ul>
     </div>
-    <!-- Footer Links -->
-  
-    <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">© 2019 Bébés Lutins</div>
-    <!-- Copyright -->
-  
-  </footer>
-  <!-- Footer -->
+    <div class="col-2">
+      <p class="h5">Nous suivre</p>
+      <ul class="list-unstyled">
+        <li><a href='https://www.instagram.com/bebes.lutins' class='text-light'> - Instagram</a></li>
+        <li><a href='https://www.facebook.com/bebes.lutins' class='text-light'> - Facebook</a></li>
+      </ul>
+    </div>
+    <div class="col-3">
+      <p class="h5">En savoir plus</p>
+      <ul class="list-unstyled">
+        <li><a href='' class='text-light'>Qui sommes-nous ?</a></li>
+        <li><a href='' class='text-light'>Pourquoi les couches lavables ?</a></li>
+        <li><a href='' class='text-light'>Entretien des couches lavables</a></li>
+        <li><a href='' class='text-light'>Mode d'emploi des couches lavables</a></li>
+        <li><a href='' class='text-light'>Comment s'équiper ?</a></li>
+        <li><a href='' class='text-light'>Nos revendeurs</a></li>
+      </ul>
+    </div>
+  </div>
+</footer>

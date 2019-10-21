d73d113b7c9f9f70bececb22c7217cd3b6b35cce
Florian Bernard <florianbernard@MacBook-Pro-de-Florian.local>
2019-10-21 16:44:08 +0200

    Added shopping cart item characteristic

---
+++ database/factories/ShoppingCartItemCharacteristicFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ShoppingCartItemCharacteristic;
+use Faker\Generator as Faker;
+
+$factory->define(ShoppingCartItemCharacteristic::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});

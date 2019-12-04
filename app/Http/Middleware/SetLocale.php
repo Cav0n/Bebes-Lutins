76572d74fd847f4900322955f4d24a50a9bfcc4d
Florian Bernard <cav0n@hotmail.fr>
2019-12-04 16:17:06 +0100

    added middleware that setlocale to FR for each request (for date conversion)

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        setlocale(LC_TIME, 'fr_FR.utf8');
+
+        return $next($request);
+    }
+}

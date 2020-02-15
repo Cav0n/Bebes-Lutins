0d5b1b22413bc091f7a4761f8d67b721338a7675
Florian Bernard <cav0n@hotmail.fr>
2020-02-15 23:26:30 +0100

    Added price before free shipping and share link in cart

--- resources/views/pages/shopping_cart/index.blade.php
+++ resources/views/pages/shopping_cart/index.blade.php
@@ -37,10 +37,23 @@
 
                 {{-- CART RECAP --}}
                 <div class="col-lg-4 my-2">
-                    <div id="price-recap" class="border mb-2 bg-white p-3">
-                        <p class="mb-0">{{ $cartQuantity }} produits : {{ \App\NumberConvertor::doubleToPrice($cartPrice) }}</p>
-                        <p class="mb-0">Frais de ports : {{ \App\NumberConvertor::doubleToPrice($shippingCosts) }}</p>
-                        <p>TOTAL T.T.C. : {{ \App\NumberConvertor::doubleToPrice($cartPrice + $shippingCosts) }}</p>
+                    <div id="price-recap" class="border bg-white p-3">
+                        <p class="mb-0">{{ $cart->totalQuantity }} produits : {{ \App\NumberConvertor::doubleToPrice($cart->totalPrice) }}</p>
+                        <p class="mb-0">Frais de ports : {{ \App\NumberConvertor::doubleToPrice($cart->shippingCosts) }}</p>
+                        <p class="mb-0">TOTAL T.T.C. : {{ \App\NumberConvertor::doubleToPrice($cart->totalPrice + $cart->shippingCosts) }}</p>
+                        <a class="btn btn-primary" href="/panier/livraison" role="button">Valider mon panier</a>
+                    </div>
+
+                    @if($cart->priceLeftBeforeFreeShipping > 0)
+                    <div id="price-recap" class="border mt-2 bg-white p-3">
+                        <p class="mb-0">Plus que {{ \App\NumberConvertor::doubleToPrice($cart->priceLeftBeforeFreeShipping) }} pour profiter de
+                            la livraison gratuite.</p>
+                    </div>
+                    @endif
+
+                    <div id="sharing-container" class="border mt-2 bg-white p-3">
+                        <p>Vous pouvez partager votre panier avec ce lien : </p>
+                        <p class='mb-0 font-weight-bold'>https://bebes-lutins.fr/panier/{{$cart->id}}</p>
                     </div>
                 </div>
 

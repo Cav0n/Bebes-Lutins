a7660fcc23a15e840970ece85d696263fad54836
Florian Bernard <cav0n@hotmail.fr>
2020-02-16 15:48:34 +0100

    Added payments buttons + cheque infos

--- resources/views/pages/shopping_cart/payment.blade.php
+++ resources/views/pages/shopping_cart/payment.blade.php
@@ -20,16 +20,36 @@
 
                 {{-- PAYMENT SELECTION --}}
                 <div class="col-lg-8 my-2 p-0">
-                    <form id="payment-form" action="{{ route('cart.delivery.validation') }}" method="POST" class="border bg-white p-3">
-                        @csrf
-                        <div class="form-group">
-                            <label for="payment_select">Méthode de paiement</label>
-                            <select id="payment_select" class="custom-select" name="paymentSelect">
-                                <option>Carte bancaire</option>
-                                <option>Chèque bancaire</option>
-                            </select>
+                    <div id="payment-form" class="border bg-white p-3 d-flex flex-column">
+                        <a name="credit-card-payment-btn" id="credit-card-payment-btn" class="btn btn-primary rounded-0" href="#" role="button">
+                            Payer par carte bancaire</a>
+
+                        <div class="separator my-3 border-top border-bottom">
+
                         </div>
-                    </form>
+                        <button id="cheque-infos-display-btn" type="button" class="btn btn-primary rounded-0">
+                            Payer par chèque bancaire</button>
+
+                        <div id='cheque-infos-container' class='p-3 border my-2'>
+                            <p class="mb-0">Merci d'établir votre chèque à l'ordre de : <b>ACTYPOLES</b>.<BR>
+                                <BR>
+                                Le paiement est à nous faire parvenir à :<BR>
+                                Actypoles / Bébés Lutins<BR>
+                                4, rue du 19 mars 1962<BR>
+                                63300 THIERS<BR>
+                                <BR>
+                                Montant de votre commande : <b>{{ \App\NumberConvertor::doubleToPrice($cartPrice + $shippingCosts) }}</b>.<BR>
+                                <BR>
+                                Votre commande sera traitée et expédiée à réception de votre chèque.</p>
+
+                            <div class="separator my-3 border-top border-bottom"></div>
+
+                            <a name="cheque-payment-btn" id="cheque-payment-btn" class="btn btn-primary w-100 rounded-0" href="{{ route('order.createFromCart', ['cart' => $cart]) . '?paymentMethod=CHEQUE' }}" role="button">
+                                Valider ma commande</a>
+                        </div>
+
+                        <small class="mt-2">En cliquant sur les boutons ci-dessus vous acceptez sans réserve les conditions générales de vente.</small>
+                    </div>
                 </div>
 
                 {{-- CART RECAP --}}
@@ -38,7 +58,6 @@
                         <p class="mb-0">{{ $cart->totalQuantity }} produits : {{ \App\NumberConvertor::doubleToPrice($cart->totalPrice) }}</p>
                         <p class="mb-0">Frais de ports : {{ \App\NumberConvertor::doubleToPrice($cart->shippingCosts) }}</p>
                         <p class="mb-0">TOTAL T.T.C. : {{ \App\NumberConvertor::doubleToPrice($cart->totalPrice + $cart->shippingCosts) }}</p>
-                        <button class="btn btn-primary w-100 rounded-0 mt-2" role="submit" form="new-address-form">Payer</button>
                     </div>
 
                     @if($cart->priceLeftBeforeFreeShipping > 0)
@@ -63,10 +82,10 @@
 
 @section('scripts')
     $(document).ready(function(){
-        $('#delivery-choice').hide();
+        $('#cheque-infos-container').hide();
 
-        $('#sameAddresses').on('click', function(){
-            $('#delivery-choice').toggle()
+        $('#cheque-infos-display-btn').on('click', function(){
+            $('#cheque-infos-container').toggle();
         });
     });
 @endsection

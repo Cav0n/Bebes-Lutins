e4a47b303aefe519090445b0d0632395002c232c
Florian Bernard <cav0n@hotmail.fr>
2020-02-18 00:02:08 +0100

    Childs in category dropdown menu

--- resources/views/components/utils/categories/dropdown_menu.blade.php
+++ resources/views/components/utils/categories/dropdown_menu.blade.php
@@ -1,13 +1,44 @@
 <div class="row mx-0">
     <div class="border-right px-3 d-flex flex-column" style="width: 18rem;">
         @foreach ($categories as $category)
-            <a class="mb-0" href='{{route('category', ['category' => $category->id])}}'>{{$category->name}}</a>
+            @if (null == $category->parentId)
+                <a class="mb-0 category-selector" href='#' id="{{ $category->id }}">{{ $category->name }}</a>
+            @endif
         @endforeach
     </div>
     <div class="col">
         @foreach ($categories as $category)
-            <h1 id='h1-{{$category->id}}' @if($category->id !== $categories->first()->id) style='display:none' @endif>
-                {{$category->name}}</h1>
+            <div id='cc-{{ $category->id }}' class="category-container">
+                <a id='h1-{{$category->id}}' class="h1" href="{{ route('category', ['category' => $category->id]) }}">
+                    {{$category->name}}</a>
+
+                <div class="sub-categories row m-0">
+                    @foreach ($category->child as $child)
+                        <div class="card col-xl-2 sub-category">
+                            <img src="{{ asset('images/utils/question-mark.png') }}" alt="" class="card-img-top">
+                            <div class="card-body">
+                                <a href="{{ route('category', ['category' => $child->id]) }}" class="card-text">{{$child->name}}</a>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         @endforeach
     </div>
 </div>
+
+<script>
+        categoryContainer = $('.category-container');
+
+        $(document).ready(function(){
+            categoryContainer.hide();
+            categoryContainer.first().show();
+
+            $('.category-selector').click(function(){
+                categoryContainer.hide();
+                $('#cc-' + this.id).show();
+                console.log('#cc-' + this.id);
+            });
+        });
+
+</script>

2751e0598101df19ed556fd1d7f98639c43bc319
Florian Bernard <cav0n@hotmail.fr>
2020-02-18 23:31:44 +0100

    Added modal on product added to cart

--- resources/views/components/modal/product_added_to_cart.blade.php
+++ resources/views/components/modal/product_added_to_cart.blade.php
@@ -3,17 +3,23 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Produit ajouté au panier !</h5>
+                <h5 class="modal-title">Produit ajouté avec succés !</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
             </div>
-            <div class="modal-body">
-                <p class="modal-product-id"></p>
+            <div class="modal-body row">
+                <div class="col-4">
+                    <img class="modal-product-image w-100">
+                </div>
+                <div class="col-8">
+                    <p class="modal-product-name font-weight-bold"></p>
+                    <p class="modal-product-price"></p>
+                </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save</button>
+                <a href="#"class="btn btn-outline-primary" data-dismiss="modal">Continuer mes achats</a>
+                <a href="{{ route('cart') }}" class="btn btn-secondary">Voir mon panier</a>
             </div>
         </div>
     </div>
@@ -22,9 +28,16 @@
 <script>
     $(document).on("click", ".add-to-cart", function () {
         var productId = $(this).data('id');
-        $("#product-added-modal .modal-product-id").text(productId)
-        // As pointed out in comments,
-        // it is unnecessary to have to manually call the modal.
-        // $('#addBookDialog').modal('show');
+
+        let result = ''
+        fetch('/api/product/' + productId)
+        .then(res => res.json()) // on récupère le corps au format text.
+        .then(text => {
+            price = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(text.data.price)
+
+            $("#product-added-modal .modal-product-name").text(text.data.name);
+            $("#product-added-modal .modal-product-image").attr('src', text.data.images[0].url);
+            $("#product-added-modal .modal-product-price").text(price);
+        })
     });
 </script>

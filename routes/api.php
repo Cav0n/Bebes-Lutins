b73db84490dabf4031bc8edf36b594ef32abc41e
Florian Bernard <cav0n@hotmail.fr>
2020-02-18 23:31:18 +0100

    Named product api route

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/product/{product}', 'ProductController@apiGet');
+Route::get('/product/{product}', 'ProductController@apiGet')->name('api.product');

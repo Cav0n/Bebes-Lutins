4c556a3bdc7f6289f6ab5b7fd119543beafdd979
Florian Bernard <cav0n@hotmail.fr>
2020-02-18 18:50:58 +0100

    Fixed shipping address

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -68,7 +68,7 @@ class CartController extends Controller
         $billingAddressId = $addressController->storeArray($request->input('billing'), Auth::user());
 
         $shippingAddressId = null;
-        if($request->input('sameAddress')){
+        if(null === $request->input('sameAddress') || !$request->input('sameAddress')){
             $shippingAddressId = $addressController->storeArray($request->input('shipping'), Auth::user());
         }
 

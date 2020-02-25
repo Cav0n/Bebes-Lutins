ec8bbf8c0e13752331fc3ecf5981d1f9dbc03abc
Florian Bernard <cav0n@hotmail.fr>
2020-02-25 23:14:17 +0100

    Swiper and text update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,14 +13,13 @@
         <div class="col-lg-8 col-xl-7 col-xxl-6 col-xxxl-5">
 
             <!-- Slider main container -->
-            <div class="swiper-container">
+            <div class="swiper-container d-none d-lg-flex">
                 <!-- Additional required wrapper -->
                 <div class="swiper-wrapper">
                     <!-- Slides -->
                     <div class="swiper-slide"><img src="{{ asset('images/caroussel/1.jpg') }}" alt="Caroussel"></div>
                     <div class="swiper-slide"><img src="{{ asset('images/caroussel/2.jpg') }}" alt="Caroussel"></div>
                     <div class="swiper-slide"><img src="{{ asset('images/caroussel/3.jpg') }}" alt="Caroussel"></div>
-                    ...
                 </div>
                 <!-- If we need pagination -->
                 <div class="swiper-pagination"></div>
@@ -36,7 +35,7 @@
             <h1 class="h1 font-weight-bold">
                 Bébés Lutins, le spécialiste de la couche lavable écologique et écocitoyenne pour bébé.
             </h1>
-            <p class="h5">
+            <p class="h5 text-justify">
                 Bébés Lutins vous propose sa gamme de couches lavables pour bébé et accessoires,
                 confectionnés en France par nos couturières. Nous sélectionnons soigneusement les tissus
                 certifiés Oeko-Tex pour offrir une couche lavable écologique qui respecte la peau de bébé.

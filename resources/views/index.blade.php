29d3c3791ef6be7aa76d4a60b9d71d9478854de5
Florian Bernard <cav0n@hotmail.fr>
2020-02-28 23:50:43 +0100

    Updated carousel
    CSS updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 @php
     $products = App\Product::all();
+    $carouselItems = \App\CarouselItem::all();
 @endphp
 
 @extends('templates.default')
@@ -10,29 +11,27 @@
 
 <div class="container-fluid my-5">
     <div class="row justify-content-center">
-        <div class="col-lg-8 col-xl-7 col-xxl-6 col-xxxl-5">
+        <div class="col-lg-9 col-xl-8 col-xxl-6 col-xxxl-5">
 
-            <div id="mainCarousel" class="carousel slide text-dark" data-ride="carousel">
+            <div id="mainCarousel" class="carousel slide text-dark mb-3 d-none d-lg-flex" data-ride="carousel">
                 <ol class="carousel-indicators">
-                    <li data-target="#mainCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#mainCarousel" data-slide-to="1"></li>
-                    <li data-target="#mainCarousel" data-slide-to="2"></li>
+                    @php $index = 0; @endphp
+                    @foreach ($carouselItems as $item)
+                        <li data-target="#mainCarousel" data-slide-to="{{ $index }}" @if (0 == $index) class="active" @endif></li>
+                        @php $index++; @endphp
+                    @endforeach
                 </ol>
                 <div class="carousel-inner">
-                    @php
-                        $index = 0;
-                    @endphp
-                    @foreach (\App\CarouselItem::all() as $item)
-                    <div class="carousel-item @if(0==$index) active @endif">
+                    @php $index = 0; @endphp
+                    @foreach ($carouselItems as $item)
+                    <div class="carousel-item @if (0 == $index) active @endif">
                         <img src="{{$item->image->url}}" alt="{{$item->image->name}}" style="width:100%">
                         <div class="carousel-caption d-none d-md-block">
                             <h5 class="text-dark">{{$item->title}}</h5>
                             <p class="text-dark">{{$item->description}}</p>
                         </div>
                     </div>
-                    @php
-                        $index++;
-                    @endphp
+                    @php $index++; @endphp
                     @endforeach
                 </div>
                 <a class="carousel-control-prev" href="#mainCarousel" role="button" data-slide="prev">

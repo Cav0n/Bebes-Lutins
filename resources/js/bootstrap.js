849dce833f0140c2fd372d295d2ad88bee8f6857
Florian Bernard <cav0n@hotmail.fr>
2020-03-07 12:48:28 +0100

    Removed swiper js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,7 +22,6 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.Swiper = require('swiper');
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- resources/views/pages/product/index.blade.php
+++ resources/views/pages/product/index.blade.php
@@ -6,7 +6,7 @@
 
 <div class="container-fluid my-5">
     <div class="row justify-content-center">
-        <div class="col-lg-8">
+        <div class="col-lg-9 col-xl-8 col-xxl-6 col-xxxl-5">
             <p>{!! $product->breadcrumb !!}</p>
             <h1>{{$product->name}}</h1>
             <p>{{$product->description}}</p>

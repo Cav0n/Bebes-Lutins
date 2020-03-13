7c425c2bb0dfb53ba1c49526eba0b5ad9939a349
Florian Bernard <cav0n@hotmail.fr>
2020-03-13 21:14:36 +0100

    Added french translations

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ces informations ne sont liés à aucun compte Bébés Lutins.',
+    'password.failed' => 'Le mot de passe est incorrect.',
+    'throttle' => 'Vous avez effectué trop de tentative de connexion. Merci de réessayer dans :seconds secondes.',
+
+];

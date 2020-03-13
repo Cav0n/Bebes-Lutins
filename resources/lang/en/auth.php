c44b7870a7fee690d82688a1c8169b218cf656f9
Florian Bernard <cav0n@hotmail.fr>
2020-03-13 21:14:12 +0100

    Added password failed string

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -14,6 +14,7 @@ return [
     */
 
     'failed' => 'These credentials do not match our records.',
+    'password.failed' => 'Password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];

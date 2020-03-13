696b28ea58027017864866e68e57aa2aab18205f
Florian Bernard <cav0n@hotmail.fr>
2020-03-13 21:14:54 +0100

    Added password error at login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -6,33 +6,34 @@
 
 <div class="container-fluid py-5">
     <div class="row justify-content-center py-xl-5">
-        <div class="col-11 col-md-10 col-lg-6 col-xl-5 col-xxl-4 col-xxxl-3 py-3 border bg-white">
-            <h1>Connexion</h1>
-            @if ($errors->any())
+        <div class="col-11 col-md-10 col-lg-6 col-xl-5 col-xxl-4 col-xxxl-3 p-0">
+            @if ($errors->has('throttle'))
                 <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
+                    <p class="mb-0">{{ $errors->first('throttle') }}</p>
                 </div>
             @endif
-            <form action="{{route('login')}}" method="POST">
-                @csrf
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" name="email" id="email" aria-describedby="helpEmail">
-                    <small id="helpEmail" class="form-text text-muted">
-                       <a href="{{route('registration')}}">Vous n'avez pas encore de compte ?</a>
-                    </small>
-                </div>
-                <div class="form-group">
-                    <label for="password">Mot de passe</label>
-                    <input type="password" class="form-control" name="password" id="password" aria-describedby="helpPassword">
-                    <small id="helpPassword" class="form-text text-muted">Vous avez perdu votre mot de passe ?</small>
-                </div>
-                <button type="submit" class="btn btn-primary">Se connecter</button>
-            </form>
+            <div class="p-3 border bg-white">
+                <h1>Connexion</h1>
+
+                <form action="{{route('login')}}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="email" id="email" aria-describedby="helpEmail" value="{{ old('email') }}">
+                        {!! $errors->has('email') ? "<div class='invalid-feedback'>" . $errors->first('email') . "</div>" : '' !!}
+                        <small id="helpEmail" class="form-text text-muted">
+                           <a href="{{route('registration')}}">Vous n'avez pas encore de compte ?</a>
+                        </small>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Mot de passe</label>
+                        <input type="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" name="password" id="password" aria-describedby="helpPassword">
+                        {!! $errors->has('password') ? "<div class='invalid-feedback'>" . $errors->first('password') . "</div>" : '' !!}
+                        <small id="helpPassword" class="form-text text-muted">Vous avez perdu votre mot de passe ?</small>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Se connecter</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>

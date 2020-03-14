7755611823e31160d7430a204fd06c0a83737285
Florian Bernard <cav0n@hotmail.fr>
2020-03-14 09:12:34 +0100

    Fixed zoom on input in safari

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -6,7 +6,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- App CSS --}}

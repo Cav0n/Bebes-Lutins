a59df18381afdac595914499200a53376affce2e
Florian Bernard <cav0n@hotmail.fr>
2020-03-16 10:51:41 +0100

    Added trash icon in cart item
    + Added left margin to add to cart btn in product page

--- resources/views/components/utils/cart/item.blade.php
+++ resources/views/components/utils/cart/item.blade.php
@@ -8,22 +8,22 @@
             <p class="mb-0">Prix unitaire : {{ \App\NumberConvertor::doubleToPrice($item->product->price) }}</p>
         </div>
 
-        <div class='spinner-container ld-over' data-itemId='{{ $item->id }}' data-oldquantity='{{ $item->quantity }}' data-price='{{$item->product->price}}' style="width:fit-content">
-            <input
-            type="number"
-            class="form-control input-spinner cart-item-quantity"
-            name="quantity"
-            id="quantity"
-            aria-describedby="helpQuantity"
-            value="{{ $item->quantity }}"
-            max="20"
-            min="0">
-            <div class="ld ld-ring ld-spin"></div>
+        <div class="d-flex flex-wrap">
+            <div class='spinner-container ld-over' data-itemId='{{ $item->id }}' data-oldquantity='{{ $item->quantity }}' data-price='{{$item->product->price}}' style="width:fit-content">
+                <input
+                type="number"
+                class="form-control input-spinner cart-item-quantity"
+                name="quantity"
+                id="quantity"
+                aria-describedby="helpQuantity"
+                value="{{ $item->quantity }}"
+                max="20"
+                min="0">
+                <div class="ld ld-ring ld-spin"></div>
+            </div>
+            <a class="d-flex flex-column justify-content-center ml-2" href="{{ route('cart.item.delete', ['cartItem'=>$item->id]) }}" role="button" style="width: fit-content">
+                <img style="width: 1.4rem;height:1.4rem;" src="{{ asset('images/icons/trash.svg') }}" alt="Supprimer"></a>
         </div>
-
-
-        <a class="btn btn-danger p-0 px-2 mt-2" href="{{ route('cart.item.delete', ['cartItem'=>$item->id]) }}" role="button" style="width: fit-content">
-            Supprimer</a>
     </div>
     <div class="col-3 col-sm-2 col-xxl-1 d-flex flex-column justify-content-center border-left my-2 px-1">
         <p class="mb-0 text-center item-total-price">{{ \App\NumberConvertor::doubleToPrice($item->product->price * $item->quantity) }}</p>
--- resources/views/pages/product/index.blade.php
+++ resources/views/pages/product/index.blade.php
@@ -19,7 +19,7 @@
                     <h2 class="h4">{{ \App\NumberConvertor::doubleToPrice($product->price) }}</h2>
                     <div class="d-flex my-2">
                         <input type="number" class="form-control input-spinner quantity-spinner" name="quantity" id="quantity" aria-describedby="helpQuantity" min="1" max="{{ $product->stock }}" value="1">
-                        <button type="button" class="btn btn-primary add-to-cart rounded-0" data-id="{{ $product->id }}" data-toggle="modal" data-quantity="1" data-cart_id="{{ session()->get('shopping_cart')->id }}" data-target="#product-added-modal">
+                        <button type="button" class="btn btn-primary add-to-cart rounded-0 ml-3" data-id="{{ $product->id }}" data-toggle="modal" data-quantity="1" data-cart_id="{{ session()->get('shopping_cart')->id }}" data-target="#product-added-modal">
                             Ajouter au panier</button>
                     </div>
 

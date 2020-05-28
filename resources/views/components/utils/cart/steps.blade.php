9d5ec5dda14927d6cbf039d4ff438ebaeb3a6978
Florian Bernard <fbernard@openstudio.fr>
2020-05-28 17:08:46 +0200

    Added clickable cart steps

--- resources/views/components/utils/cart/steps.blade.php
+++ resources/views/components/utils/cart/steps.blade.php
@@ -1,6 +1,6 @@
 <div class="steps-container d-flex justify-content-center w-100">
 
-    <div class="step {{ $cartStep >= 1 ? 'active' : null }}">
+    <a class="step {{ $cartStep >= 1 ? 'active' : null }}" @if($cartStep > 1) href="{{ route('cart') }}" @endif>
         <p class="step-number text-center bg-white rounded-circle d-flex flex-column justify-content-center mb-0 mx-auto">
             @if ($cartStep >= 2)
             <img src="{{ asset('images/icons/success-bw.svg') }}" alt="success">
@@ -8,15 +8,15 @@
             1
             @endif
         </p>
-        <p class="text-center">
+        <p class="text-center text-dark">
             Panier</p>
-    </div>
+    </a>
 
     <div>
         <p class="step-separator mb-0 mx-4"> - </p>
     </div>
 
-    <div class="step {{ $cartStep >= 2 ? 'active' : null }}">
+    <a class="step {{ $cartStep >= 2 ? 'active' : null }}" @if($cartStep > 2) href="{{ route('cart.delivery') }}" @endif>
         <p class="step-number text-center bg-white rounded-circle d-flex flex-column justify-content-center mb-0 mx-auto text-center">
             @if ($cartStep >= 3)
             <img src="{{ asset('images/icons/success-bw.svg') }}" alt="success">
@@ -24,15 +24,15 @@
             2
             @endif
         </p>
-        <p class="text-center">
+        <p class="text-center text-dark">
             Livraison</p>
-    </div>
+    </a>
 
     <div>
         <p class="step-separator mb-0 mx-4"> - </p>
     </div>
 
-    <div class="step {{ $cartStep >= 3 ? 'active' : null }}">
+    <a class="step {{ $cartStep >= 3 ? 'active' : null }}" @if($cartStep > 3) href="{{ route('cart.payment') }}" @endif>
         <p class="step-number text-center bg-white rounded-circle d-flex flex-column justify-content-center mb-0 mx-auto">
             @if ($cartStep >= 4)
             <img src="{{ asset('images/icons/success-bw.svg') }}" alt="success">
@@ -40,8 +40,8 @@
             3
             @endif
         </p>
-        <p class="text-center">
+        <p class="text-center text-dark">
             Paiement</p>
-    </div>
+    </a>
 
 </div>
